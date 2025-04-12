fa68c2a5a1d6b079d129c65c204ffa33826c4343
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2025-04-13 00:59:22 +0530

    database connection doen

--- smartBuild/app.js
+++ smartBuild/app.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
+
+
+const app =express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from the app.js route! '});
+
+});
+
+module.exports = app;
--- smartBuild/index.js
+++ smartBuild/index.js
@@ -1,14 +1,12 @@
 const app = require('./app');
-const db = require('./config/db')
+const db = require('./config/db'); // Assuming your db connection is initialized here
 
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello World!!!!!1")
-})
+app.get('/', (req, res) => {
+    res.send("Hello World!!!!!1");
+});
 
-
-
-app.listen(port,()=>{
-    console.log('Server Listening on port http://localhost:${port}');
-})
+app.listen(port, () => {
+    console.log(`Server Listening on port http://localhost:${port}`);
+});
