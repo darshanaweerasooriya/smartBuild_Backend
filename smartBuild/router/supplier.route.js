9093a65c8e9c313be6892d989e17595d812f73c3
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2025-04-13 20:39:37 +0530

    Suplier login done

--- smartBuild/router/supplier.route.js
+++ smartBuild/router/supplier.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const supplierContoller = require("../controller/suppliers.contorller");
 
 router.post('/supplierRegister', supplierContoller.register);
-
+router.post('/suplogin', supplierContoller.suplogin);
+router.post('/getSupplier', supplierContoller.getsup);
 
 module.exports = router;
--- smartBuild/services/supplier.service.js
+++ smartBuild/services/supplier.service.js
@@ -1,20 +1,31 @@
 const supplierModel  = require('../model/supplier.model');
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const SECRET_KEY = "your_jwt_scret";
 
 class supplierService {
     static async registerSupplier(firstName, phoneNumb, companyName, email, password){
         try {
-            const exisitngSupplier = await supplierModel.findOne({ email });
-            if (exisitngSupplier){
+            const existingSupplier = await supplierModel.findOne({ email });
+            if (existingSupplier){
                 throw new Error("Email already registered.");
             }
 
-            const creatSupplier = new supplierModel({firstName, phoneNumb, companyName, email, password });
-            const savedSupplier = await creatSupplier.save();
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
 
-            const token = jwt.sign({ id:savedSupplier._id}, SECRET_KEY, { expiresIn: "1d"});
+            const createSupplier = new supplierModel({
+                firstName,
+                phoneNumb,
+                companyName,
+                email,
+                password: hashedPassword
+            });
+
+            const savedSupplier = await createSupplier.save();
+
+            const token = jwt.sign({ id: savedSupplier._id }, SECRET_KEY, { expiresIn: "1d" });
 
             return {
                 user: {
@@ -22,7 +33,6 @@ class supplierService {
                     phoneNumb: savedSupplier.phoneNumb,
                     companyName: savedSupplier.companyName,
                     email: savedSupplier.email,
-                    password: savedSupplier.password
                 },
                 token
             };
@@ -31,6 +41,42 @@ class supplierService {
         }
     }
 
+    static async checksuplier(email){
+        try{
+            const supplier = await supplierModel.findOne({ email });
+            return supplier;
+        } catch (error){
+            throw error;
+        }
+    }
+
+    static async generateToken(tokenData, secretKey, jwt_expire){
+        return jwt.sign(tokenData, secretKey, { expiresIn: jwt_expire });
+    }
+
+    static async verifyToken(token, secretKey){
+        try {
+            return jwt.verify(token, secretKey);
+        } catch (error){
+            throw error;
+        }
+    }
+
+    static async updatePassword(email, newPassword) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            const hashPassword = await bcrypt.hash(newPassword, salt);
+
+            return await supplierModel.findOneAndUpdate({ email }, { password: hashPassword }, { new: true });
+        } catch (error){
+            throw error;
+        }
+    }
+
+    static async getsuptdata(email){
+        const supdata = await supplierModel.find({ email });
+        return supdata;
+    }
 }
 
 module.exports = supplierService;
