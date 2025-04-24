aa4b2ac33d14440b5c92a36f51f89b9a81103f9c
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2025-04-24 21:26:53 +0530

    update, delete,fetch

--- smartBuild/controller/addWorkers.contorller.js
+++ smartBuild/controller/addWorkers.contorller.js
@@ -11,8 +11,42 @@ const addWorker = async (req, res) => {
         console.error(error);
         res.status(500).json({ message: 'Error adding workers' });
     }
+};
+
+const updateWorker = async (req, res) => {
+    try{
+        const updatedWorker = await workerService.updateWorker(req.params.id, req.body, req.file);
+        if (!updateWorker) {
+            return res.status (404).json({ message: 'Product not found ' });
+        }
+        res.status(200).json ({
+            message: 'Worker updated successfully',
+            product: updatedWorker
+        });
+    }catch (error){
+        console.error(error);
+        res.status(500).json({ message: 'Error updating workers'});
+    }
+}
+
+const deleteWork = async (req, res) => {
+    try {
+        const deletedWork = await workerService.deleteWork(req.params.id);
+        if (!deletedWork){
+            return res.status(404).json({catch (error){
+                console.error(error);
+                res.status(500).json({ message: 'Error Delete worker'});
+            }})
+                }
+        
+            }catch (error) {
+                console.error(error);
+                res.status(500).json({ message: 'Error deleting worker' });
+            }
 }
 
 module.exports = {
     addWorker,
+    updateWorker,
+    deleteWork
 }
--- smartBuild/router/addWorkers.route.js
+++ smartBuild/router/addWorkers.route.js
@@ -7,5 +7,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.post('/addWorker',upload.single('image'), workerController.addWorker);
+router.delete('delete/:id', workerController.deleteWork);
+router.put('/update/:id', upload.single('image'), productController.updateProduct);
 
 module.exports = router;
